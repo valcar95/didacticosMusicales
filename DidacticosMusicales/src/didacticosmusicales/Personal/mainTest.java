6d9e0697834b7fb68f1c36c3a4fc3f346bd43d8f
Victor Cardona <victor.cardona@xpertgroup.co>
2018-05-26 11:31:17 -0500

    eliminación de carpeta por fuera modulo produccion

--- DidacticosMusicales/src/didacticosmusicales/Personal/mainTest.java
+++ DidacticosMusicales/src/didacticosmusicales/Personal/mainTest.java
@@ -7,6 +7,10 @@ package didacticosmusicales.Personal;
 
 import didacticosmusicales.Personal.Aplicacion.DTO.AsignacionDTO;
 import didacticosmusicales.Personal.Aplicacion.Implementaciones.AsignacionCtrl;
+import didacticosmusicales.Personal.Aplicacion.Implementaciones.TrabajadorCtrl;
+import didacticosmusicales.Personal.Aplicacion.Utilidades.Response;
+import didacticosmusicales.Personal.Dominio.Entidades.Trabajador;
+import didacticosmusicales.Personal.Infraestructura.Repositorio.TrabajadorRepositorio;
 
 /**
  *
@@ -15,9 +19,15 @@ import didacticosmusicales.Personal.Aplicacion.Implementaciones.AsignacionCtrl;
 public class mainTest {
     
     public static void main(String[] str){
+        
+        
+        TrabajadorCtrl t=new TrabajadorCtrl();
+        
+        Response<Trabajador> tt= t.ObtenerTrabajadorPorCedula("aaaaa");
+        
         AsignacionCtrl ctrl= new AsignacionCtrl();
 
-        AsignacionDTO a= new AsignacionDTO();
+       /* AsignacionDTO a= new AsignacionDTO();
 
         a.setCedulaTrabajador("123456");
         a.setCedulaCliente("1232234");
@@ -28,7 +38,7 @@ public class mainTest {
         a.setHoraFin("11:00 am");
         a.setIdServicio(1);
         
-        ctrl.RegistrarAsignacionCliente(a);
+        ctrl.RegistrarAsignacionCliente(a);*/
     }
     
     
